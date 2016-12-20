db56a0fa50b5a998dc49dca0cff76aef45a3f54a
Thomas <thomas.hansson85@gmail.com>
2016-12-20 15:18:35 +0100

    case

--- src/Jimmy/AddressBookGroup/core/CommandInterpreter.java
+++ src/Jimmy/AddressBookGroup/core/CommandInterpreter.java
@@ -5,7 +5,14 @@ public class CommandInterpreter{
     public void interpret(CommandLine commandLine) throws InvalidCommandException {
         switch(commandLine.command){
             case("add"):
-                System.out.println("Korrekt" + commandLine.parameters);
+
+            case("delete"):
+
+            case("list"):
+
+            case("search"):
+
+            case("quit"):
 
         }
     }
