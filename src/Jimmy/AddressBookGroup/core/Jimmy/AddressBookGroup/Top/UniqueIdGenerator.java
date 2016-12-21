c3bbf26c52cae64db8cdc69e753961135e24fef1
Thomas <thomas.hansson85@gmail.com>
2016-12-21 11:30:00 +0100

    UUID-generator

--- src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/UniqueIdGenerator.java
+++ src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/UniqueIdGenerator.java
@@ -1,4 +1,10 @@
 package Jimmy.AddressBookGroup.core.Jimmy.AddressBookGroup.Top;
 
+import java.util.UUID;
+
 public class UniqueIdGenerator {
+
+    public static UUID id(){
+        return UUID.randomUUID();
+    }
 }
