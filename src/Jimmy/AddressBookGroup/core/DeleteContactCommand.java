84f7cc87cee61cd66bcb3bcc7d19589b85000229
Peder Waern <pederwaern@hotmail.com>
2016-12-21 13:35:49 +0100

    changed contatformatter

--- src/Jimmy/AddressBookGroup/core/DeleteContactCommand.java
+++ src/Jimmy/AddressBookGroup/core/DeleteContactCommand.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class DeleteContactCommand implements Command {
 
+    //TODO testa delete funktion
 
     final static String NAME = "delete";
     final static String DESCRIPTION = "delete contact";
