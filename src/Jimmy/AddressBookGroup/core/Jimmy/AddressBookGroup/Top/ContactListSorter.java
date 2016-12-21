d710c487da87aa705ce17c281052c84faa3e66f5
Peder Waern <pederwaern@hotmail.com>
2016-12-21 12:59:22 +0100

    changed registry delete method

--- src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/ContactListSorter.java
+++ src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/ContactListSorter.java
@@ -1,9 +1,14 @@
 package Jimmy.AddressBookGroup.core.Jimmy.AddressBookGroup.Top;
 
+import java.util.List;
+
 public class ContactListSorter {
-/*
+
     public List<Contact> sort(List<Contact> contacts){
 
+        contacts.sort((contactOne, contactTwo) -> contactOne.getFirstName().compareToIgnoreCase(contactTwo.getFirstName()));
 
-    }*/
+        return contacts;
+    }
+
 }
