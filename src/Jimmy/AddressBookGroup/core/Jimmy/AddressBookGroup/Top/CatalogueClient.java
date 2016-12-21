c35d8a0e387e1bf2be002cdb59b7cc879f0258b6
Thomas <thomas.hansson85@gmail.com>
2016-12-21 15:10:40 +0100

    Added RemoteRegistry

--- src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/CatalogueClient.java
+++ src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/CatalogueClient.java
@@ -5,7 +5,13 @@ public class CatalogueClient {
     private String host;
     private int port;
 
-    public void connect(){}
+    public CatalogueClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void connect(){
+    }
 
     public void sendRequest(String string){}
 
