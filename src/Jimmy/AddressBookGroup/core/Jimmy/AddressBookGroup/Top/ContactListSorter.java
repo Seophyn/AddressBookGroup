69c1aab4e20865ccccb2a5093e6f58e7854f081d
Peder Waern <pederwaern@hotmail.com>
2016-12-21 12:57:09 +0100

    Merge branch 'master' of /Users/pederwaern/IdeaProjects/AddressBookGroup with conflicts.

--- src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/ContactListSorter.java
+++ src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/ContactListSorter.java
@@ -1,15 +1,9 @@
 package Jimmy.AddressBookGroup.core.Jimmy.AddressBookGroup.Top;
 
-import java.util.Comparator;
-import java.util.List;
-
 public class ContactListSorter {
-
+/*
     public List<Contact> sort(List<Contact> contacts){
 
-        contacts.sort((contactOne, contactTwo) -> contactOne.getFirstName().compareToIgnoreCase(contactTwo.getFirstName()));
 
-        return contacts;
-    }
-
+    }*/
 }
