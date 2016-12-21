867a740fd4191ae20eed7858abdd6ebe9bd4ecff
Peder Waern <pederwaern@hotmail.com>
2016-12-21 11:23:51 +0100

    localcontact updated

--- src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/ContactDetails.java
+++ src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/ContactDetails.java
@@ -4,4 +4,16 @@ public class ContactDetails {
     String firstName;
     String lastName;
     String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
