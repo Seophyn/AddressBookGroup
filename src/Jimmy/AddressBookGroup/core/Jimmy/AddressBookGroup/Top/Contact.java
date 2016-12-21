a968fb49a4ba8a2c211c2ea930ce3ba60230e14f
Thomas <thomas.hansson85@gmail.com>
2016-12-21 09:32:35 +0100

    Contact

--- src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/Contact.java
+++ src/Jimmy/AddressBookGroup/core/Jimmy/AddressBookGroup/Top/Contact.java
@@ -4,5 +4,8 @@ package Jimmy.AddressBookGroup.core.Jimmy.AddressBookGroup.Top;
  * Created by Jimmy on 20/12/2016.
  */
 public interface Contact {
-
+    String getId();
+    void getFirstName();
+    void getLastName();
+    void getEmailAddress();
 }
